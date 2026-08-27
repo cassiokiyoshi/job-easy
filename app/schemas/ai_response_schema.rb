20973c7ed9a78390d8d016e51c9d377237c4077f
Gifta Santosa <giftaputri.santosa@gmail.com>
2026-08-27 21:55:35 +0900

    Fixed button alerts

--- app/schemas/ai_response_schema.rb
+++ app/schemas/ai_response_schema.rb
@@ -1,12 +1,12 @@
 require "ruby_llm/schema"
 class AiResponseSchema < RubyLLM::Schema
   string :order_advice,
-         description: "One sentence instruction to recommend user about the position inside a resume. With a specific job title and JD, most relevant project or work experience (if any) needs to be near the top of the resume page."
+         description: "One sentence instruction to recommend user about the position inside a resume. With a specific job title and JD, most relevant project or work experience (if any) needs to be near the top of the resume page. Use a simple markdown, but cannot be a table or pipes"
 
   string :summary_advice,
-         description: "One sentence instruction to recommend user about the top executive summary. Given a specific job title and JD, the tech stacks should match resume in bold. Summary should not be no longer than 5 lines. Better put the years of experience on the first line (example. With X months of experience in Java....)"
+         description: "One sentence instruction to recommend user about the top executive summary. Given a specific job title and JD, the tech stacks should match resume in bold. Summary should not be no longer than 5 lines. Better put the years of experience on the first line (example. With X months of experience in Java....). Use a simple markdown, but cannot be a table or pipes"
 
   array :addutional_advice,
         of: :string,
-        description: "More recommedations to recommend user about the content of a tech resume, given a specific job title and JD. Array should consist of maximum 5 elements"
+        description: "More recommedations to recommend user about the content of a tech resume, given a specific job title and JD. Array should consist of maximum 5 elements. Use a simple markdown, but cannot be a table or pipes"
 end
