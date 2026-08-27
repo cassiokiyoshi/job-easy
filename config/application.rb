b10f76cf53d6aa22cbbac5d2d9e1cfe0a910af6a
Gifta Santosa <giftaputri.santosa@gmail.com>
2026-08-27 11:44:33 +0900

    Added more config

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module JobEasy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :solid_queue
   end
 end
