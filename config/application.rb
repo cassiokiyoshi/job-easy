52e735f4cbebe6895397c2d7bdcc5f5597f645a1
Gifta Santosa <giftaputri.santosa@gmail.com>
2026-09-01 11:06:42 +0900

    Added Tokyo Timezone

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module JobEasy
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_job.queue_adapter = :solid_queue
+    config.time_zone = "Tokyo"
   end
 end
